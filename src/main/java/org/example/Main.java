56b1777db65b021aa3e5b17e7620004d7fb3d691
Rita M <kolotvinova03@bk.ru>
2023-10-12 23:56:07 +0300

    update main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,13 +6,10 @@ public class Main {
     public static void main(String[] args) {
         Connect connect = new Connect();
         System.out.println(connect.getUsers());
-
         User userToAdd = new User(3L, "Bob", "Kelso", (byte) 55);
         System.out.println(connect.save(userToAdd));
-        System.out.println(connect.getUsers());
         User userToPatch = new User(3L, "Jonathan", "Dorian", (byte) 25);
         System.out.println(connect.patch(userToPatch));
-        System.out.println(connect.getUsers());
         System.out.println(connect.delete(3L));
         System.out.println(connect.getUsers());
     }
